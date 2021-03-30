d03d17be945dd4c2f8680befdc5c9ac4638f4bd6
Nikolay <nchernov@gmail.com>
2021-03-30 22:48:26 +0300

    simple app

--- App.js
+++ App.js
@@ -1,14 +1,19 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, View, Alert } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Open up App.js to start working on your app!!!</Text>
       <StatusBar style="auto" />
+      <Button title='GO' onPress={() => onPressButton()}/>
     </View>
   );
+}
+
+function onPressButton() {
+  Alert.alert('Simple Button pressed!!!');
 }
 
 const styles = StyleSheet.create({
